5187a66a74e2e135ba4a3673292bcec690be8f3e
karasique22 <timofeidudin@mail.ru>
2025-06-09 14:20:10 +0300

    quickfix

--- frontend/src/app/details/[slug]/page.tsx
+++ frontend/src/app/details/[slug]/page.tsx
@@ -4,6 +4,5 @@ export default async function Page(props: {
   params: Promise<{ slug: string }>;
 }) {
   const params = await props.params;
-  console.log(params);
   return <Details slug={params.slug} />;
 }
