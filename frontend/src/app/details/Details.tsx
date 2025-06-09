55192f57720db5bcb7270abd3829e281d50d09f2
karasique22 <timofeidudin@mail.ru>
2025-06-09 14:18:44 +0300

    quickfix

--- frontend/src/app/details/Details.tsx
+++ frontend/src/app/details/Details.tsx
@@ -2,7 +2,6 @@
 
 import { useFetch } from '@/hooks/useFetch';
 import { notFound, useSearchParams } from 'next/navigation';
-import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
